e7340153c187d2fc45bab95c856453f8a5d05186
radoslav.bahchevanov <radoslav.bahchevanov@gmail.com>
2017-06-26 18:46:38 +0300

    Created data connection, BaseController

---
+++ WebCalendar.App/Controllers/BaseController.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+using WebCalendar.Data;
+
+namespace MaznaMutska.App.Controllers
+{
+    [Authorize] // All controllers that inherit from this one will be available only for registered users
+    public abstract class BaseController : Controller
+    {
+        private WebCalendarDb context;
+
+        public BaseController()
+        {
+            context = new WebCalendarDb();
+        }
+
+        public BaseController(WebCalendarDb context)
+        {
+            this.context = context;
+        }
+
+
+        protected WebCalendarDb Context
+        {
+            get
+            {
+                return context;
+            }
+        }
+
+        // Authentication logic, runs for any action, marked with Authorize attribute
+        protected override void OnAuthentication(AuthenticationContext filterContext)
+        {
+            if (this.HttpContext.Request.Cookies != null)
+            {
+                var tokenCookie = this.HttpContext.Request.Cookies.Get("userToken");
+                if (tokenCookie != null && !string.IsNullOrEmpty(tokenCookie.Value))
+                {
+                    var user = Context.Users.FirstOrDefault(u => u.Token == tokenCookie.Value);
+                    if (user != null)
+                    {
+                        this.HttpContext.User = new GenericPrincipal(new GenericIdentity(user.Username), new string[] { } );
+                        this.HttpContext.Session["userToken"] = tokenCookie.Value;
+                    }
+                    else Response.SetCookie(new HttpCookie("userToken", null)); // if user token is not in the database, delete userToken in cookie
+                }
+            }
+
+            base.OnAuthentication(filterContext);
+        }
+
+    }
+}
